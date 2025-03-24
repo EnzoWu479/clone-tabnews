25dc49df36325526e45b9eb0954cdf5ef121405e
EnzoWu <75583251+EnzoWu479@users.noreply.github.com>
2025-03-24 01:09:09 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import { useRouter } from "next/router";
 
 function Home() {
-    const router = useRouter();
-    const pessoa = router.query.nome;
+  const router = useRouter();
+  const pessoa = router.query.nome;
 
-    return <h1>Oi {pessoa}! To aqui para fazer o desafio do curso.dev, e te dizer que vc é uma pessoa incrível.</h1>
+  return (
+    <h1>
+      Oi {pessoa}! To aqui para fazer o desafio do curso.dev, e te dizer que vc
+      é uma pessoa incrível.
+    </h1>
+  );
 }
 
 export default Home;
